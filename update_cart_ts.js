16446fea2bf142522cf51cdb591672c95111a073
goldenparckcp-ux <golden.parck.cp@gmail.com>
2026-07-05 20:27:44 +0200

    chore: major cleanup, remove 88 unused files, uninstall unused deps, UI/UX audit complete

--- update_cart_ts.js
+++
@@ -1,4 +0,0 @@
-﻿const fs = require('fs');
-let content = fs.readFileSync('lib/state/CartContext.tsx', 'utf8');
-content = content.replace(/name: string;/g, 'name: string;\n    name_ar?: string;');
-fs.writeFileSync('lib/state/CartContext.tsx', content, 'utf8');
--- update_lang.js
+++
@@ -1,7 +0,0 @@
-﻿const fs = require('fs');
-let content = fs.readFileSync('app/restaurant/page.tsx', 'utf8');
-
-content = content.replace(/const { t } = useTranslation\(\);/g, 'const { t, language } = useTranslation();');
-
-fs.writeFileSync('app/restaurant/page.tsx', content, 'utf8');
-console.log('Language extracted');
